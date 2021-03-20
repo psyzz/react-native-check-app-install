c1b02018e0999e37a0768fd2078e7c3256d9bce8
Jeremy Roustang <contact.glilab@gmail.com>
2021-03-20 19:04:47 +0100

    fix isAppInstalledAndroid

--- app-installed-checker.js
+++ app-installed-checker.js
@@ -1,48 +1,58 @@
-import { Linking, Platform } from 'react-native';
-import { APP_LIST } from './app-list';
-import CheckPackageInstallation from './android';
+import { Linking, Platform } from "react-native";
+import { APP_LIST } from "./app-list";
+import CheckPackageInstallation from "./android";
 
 class AppInstalledChecker {
+	static getAppList() {
+		return Object.keys(APP_LIST);
+	}
 
-    static getAppList() {
-        return Object.keys(APP_LIST);
-    }
+	static checkPackageName(packagename) {
+		return new Promise((resolve, reject) => {
+			CheckPackageInstallation.isPackageInstalled(
+				packagename,
+				(isInstalled) => {
+					resolve(isInstalled);
+				}
+			);
+		});
+	}
 
-    static checkPackageName(packagename) {
-        return new Promise((resolve, reject) => {
-            CheckPackageInstallation.isPackageInstalled(packagename, (isInstalled) => {
-                resolve(isInstalled);
-            });
-        });
-    }
+	static checkURLScheme(proto, query) {
+		return new Promise((resolve, reject) => {
+			Linking.canOpenURL(proto + "://" + query || "")
+				.then((isInstalled) => {
+					resolve(isInstalled);
+				})
+				.catch((err) => {
+					reject(err);
+				});
+		});
+	}
 
-    static checkURLScheme(proto, query) {
-        return new Promise((resolve, reject) => {
-            Linking
-                .canOpenURL(proto + '://' + query || '')
-                .then((isInstalled) => {
-                    resolve(isInstalled);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
-        });
-    }
+	static isAppInstalled(key) {
+		return Platform.select({
+			ios: () => {
+				return this.isAppInstalledIOS(key);
+			},
+			android: () => {
+				return this.isAppInstalledAndroid(key);
+			},
+		})();
+	}
 
-    static isAppInstalled(key) {
-        return Platform.select({
-            ios: () => { return this.isAppInstalledIOS(key); },
-            android: () => { return this.isAppInstalledAndroid(key); }
-        })();
-    }
+	static isAppInstalledAndroid(key) {
+		return this.checkPackageName(
+			APP_LIST[key] != null ? APP_LIST[key].pkgName : key
+		);
+	}
 
-    static isAppInstalledAndroid(key) {
-        return this.checkPackageName(key);
-    }
-
-    static isAppInstalledIOS(key) {
-        return this.checkURLScheme(APP_LIST[key].urlScheme, APP_LIST[key].urlParams);
-    }
+	static isAppInstalledIOS(key) {
+		return this.checkURLScheme(
+			APP_LIST[key].urlScheme,
+			APP_LIST[key].urlParams
+		);
+	}
 }
 
 export default AppInstalledChecker;
